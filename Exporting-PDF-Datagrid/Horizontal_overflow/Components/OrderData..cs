27a2c0c65d0c50fc5a3f98ed8d3fe1bdc6cf8881
Gayathri4135 <gayathri.palanivel@syncfusion.com>
2025-05-09 12:58:03 +0530

    Updated the pdf sample

---
+++ Exporting-PDF-Datagrid/Horizontal_overflow/Components/OrderData..cs
@@ -0,0 +1,47 @@
+﻿namespace Horizontal_overflow.Components
+{
+    public class OrderData
+    {
+        public static List<OrderData> Orders = new();
+
+        public OrderData(int orderID, string customerID, string shipCity, string shipName, string shipAddress, string shipRegion, string shipPostalCode, string shipCountry)
+        {
+            OrderID = orderID;
+            CustomerID = customerID;
+            ShipCity = shipCity;
+            ShipName = shipName;
+            ShipAddress = shipAddress;
+            ShipRegion = shipRegion;
+            ShipPostalCode = shipPostalCode;
+            ShipCountry = shipCountry;
+        }
+
+        public static List<OrderData> GetAllRecords()
+        {
+            if (Orders.Count == 0)
+            {
+                Orders.Add(new OrderData(10248, "VINET", "Reims", "Vins et alcools Chevalier", "59 rue de l'Abbaye", "SP", "51100", "France"));
+                Orders.Add(new OrderData(10249, "TOMSP", "Münster", "Toms Spezialitäten", "Luisenstr. 48", "JJ", "44087", "Germany"));
+                Orders.Add(new OrderData(10250, "HANAR", "Rio de Janeiro", "Hanari Carnes", "Rua do Paço, 67", "RJ", "05454-876", "Brazil"));
+                Orders.Add(new OrderData(10251, "VICTE", "Lyon", "Victuailles en stock", "2, rue du Commerce", "RJ", "69004", "France"));
+                Orders.Add(new OrderData(10252, "SUPRD", "Charleroi", "Suprêmes délices", "Boulevard Tirou, 255", "SP", "B-6000", "Belgium"));
+                Orders.Add(new OrderData(10253, "HANAR", "Rio de Janeiro", "Hanari Carnes", "Rua do Paço, 67", "RJ", "05454-876", "Brazil"));
+                Orders.Add(new OrderData(10254, "CHOPS", "Bern", "Chop-suey Chinese", "Hauptstr. 31", "JJ", "3012", "Switzerland"));
+                Orders.Add(new OrderData(10255, "RICSU", "Genève", "Richter Supermarkt", "Starenweg 5", "SP", "1204", "Switzerland"));
+                Orders.Add(new OrderData(10256, "WELLI", "Resende", "Wellington Importadora", "Rua do Mercado, 12", "SP", "08737-363", "Brazil"));
+                Orders.Add(new OrderData(10257, "HILAA", "San Cristóbal", "HILARION-Abastos", "Carrera 22 con Ave. Carlos Soublette #8-35", "Táchira", "5022", "Venezuela"));
+            }
+
+            return Orders;
+        }
+
+        public int OrderID { get; set; }
+        public string CustomerID { get; set; } = string.Empty;
+        public string ShipCity { get; set; } = string.Empty;
+        public string ShipName { get; set; } = string.Empty;
+        public string ShipAddress { get; set; } = string.Empty;
+        public string ShipRegion { get; set; } = string.Empty;
+        public string ShipPostalCode { get; set; } = string.Empty;
+        public string ShipCountry { get; set; } = string.Empty;
+    }
+}
---
+++ Exporting-PDF-Datagrid/Merging_Two_PDF_Memory_Stream/Components/OrderData.cs
@@ -0,0 +1,39 @@
+﻿namespace Merging_Two_PDF_Memory_Stream.Components
+{
+    public class OrderData
+    {
+        public static List<OrderData> Orders = new List<OrderData>();
+
+        public OrderData() { }
+
+        public OrderData(int OrderID, string CustomerID,  double Freight, DateTime? OrderDate)
+        {
+            this.OrderID = OrderID;
+            this.CustomerID = CustomerID;
+            this.Freight = Freight;
+            this.OrderDate = OrderDate;
+        }
+
+        public static List<OrderData> GetAllRecords()
+        {
+            if (Orders.Count == 0)
+            {
+                Orders.Add(new OrderData { OrderID = 10248, CustomerID = "VINET", Freight = 32.38, OrderDate = new DateTime(1996, 7, 4) });
+                Orders.Add(new OrderData { OrderID = 10249, CustomerID = "TOMSP", Freight = 11.61, OrderDate = new DateTime(1996, 7, 5) });
+                Orders.Add(new OrderData { OrderID = 10250, CustomerID = "HANAR", Freight = 65.83, OrderDate = new DateTime(1996, 7, 6) });
+                Orders.Add(new OrderData { OrderID = 10251, CustomerID = "VINET", Freight = 41.34, OrderDate = new DateTime(1996, 7, 7) });
+                Orders.Add(new OrderData { OrderID = 10252, CustomerID = "SUPRD", Freight = 151.30, OrderDate = new DateTime(1996, 7, 8) });
+                Orders.Add(new OrderData { OrderID = 10253, CustomerID = "HANAR", Freight = 58.17, OrderDate = new DateTime(1996, 7, 9) });
+                Orders.Add(new OrderData { OrderID = 10254, CustomerID = "CHOPS", Freight = 22.98, OrderDate = new DateTime(1996, 7, 10) });
+                Orders.Add(new OrderData { OrderID = 10255, CustomerID = "VINET", Freight = 148.33, OrderDate = new DateTime(1996, 7, 11) });
+                Orders.Add(new OrderData { OrderID = 10256, CustomerID = "HANAR", Freight = 13.97, OrderDate = new DateTime(1996, 7, 12) });
+            }
+            return Orders;
+        }
+
+        public int OrderID { get; set; }
+        public string CustomerID { get; set; }
+        public double? Freight { get; set; }
+        public DateTime? OrderDate { get; set; }
+    }
+}
